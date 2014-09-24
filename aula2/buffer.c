502a5b74c5a7ce0f8ece0abb1a2fc6cb2b1fdd09
absampaio <andrebsampaio@gmail.com>
2014-09-24 19:12:22 +0100

    aula2 incomplete

--- aula2/buffer.c
+++ aula2/buffer.c
@@ -1,12 +1,13 @@
 
 #include "buffer.h"
 
-buffer_item buffer [BUFFER_SIZE]
+buffer_item buffer [BUFFER_SIZE];
 
 int insert_item(buffer_item item){
 	printf ("producer produced %d\n", item);
 }
 
 int remove_item(buffer_item *item){
-	printf("consumer consumed %d\n", item.number);
+	struct buffer_item *b_item = (struct buffer_item *) item;
+	printf("consumer consumed %d\n", b_item->number);
 }
